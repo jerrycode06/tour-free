01a07da426da23aa48d241a051e5e54fd0626a1d
Nikhil Upadhyay <nikhil.upadhyay@oracle.com>
2024-12-07 16:44:34 +0530

    travelling to dropdown

--- src/components/pages/home/Enquiry.js
+++ src/components/pages/home/Enquiry.js
@@ -8,16 +8,24 @@ const Enquiry = () => {
         <div className="contact-form">
           <form name="trip-enquiry" method="post">
             <input type="hidden" name="form-name" value="trip-enquiry" />
-            <h5 className="contact-d-head d-flex justify-content-center">Enquire Now</h5>
+            <h5 className="contact-d-head d-flex justify-content-center">
+              Enquire Now
+            </h5>
             <div className="row">
               <div className="col-lg-6">
                 <label htmlFor="to">Travelling To</label>
-                <input
-                  type="text"
-                  placeholder="Travelling To"
-                  name="to"
-                  required
-                />
+                <select
+                  name="aiport"
+                  id="aiport"
+                  style={{ width: "90%", marginBottom: "0" }}
+                >
+                  <option value="">Select Airport</option>
+                  {AiportCodes.map((item, index) => (
+                    <option key={index} value={item.code}>
+                      {item.code}, {item.city}, {item.country}
+                    </option>
+                  ))}
+                </select>
               </div>
               <div className="col-lg-6">
                 <label htmlFor="no">No of adults</label>
@@ -39,18 +47,12 @@ const Enquiry = () => {
               </div>
               <div className="col-lg-6">
                 <label htmlFor="when">Child age</label>
-                <input type="number" placeholder="child age" name="no" required />
-              </div>
-              <div className="col-lg-6">
-              <label htmlFor="aiport">Travel From</label>
-                <select name="aiport" id="aiport" style={{width: "90%", marginBottom: "0"}}>
-                  <option value="">Select Airport</option>
-                  {AiportCodes.map((item, index) => (
-                    <option key={index} value={item.code}>
-                      {item.code}, {item.city}, {item.country}
-                    </option>
-                  ))}
-                </select>
+                <input
+                  type="number"
+                  placeholder="child age"
+                  name="no"
+                  required
+                />
               </div>
               <div className="col-lg-6">
                 <label htmlFor="when">Depart Date</label>
@@ -59,7 +61,6 @@ const Enquiry = () => {
               <div className="col-lg-12 d-flex justify-content-center">
                 <input type="submit" defaultValue="Submit Now" />
               </div>
-              
             </div>
           </form>
         </div>
